a874bf3e78d7b422fbf693516a24e34081535b3f
Bjorter <bjorkman.tero@gmail.com>
2016-01-13 13:38:46 +0200

    Harjoitus 1 initial commit

---
+++ Harj1/Harj1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anna joku numero: "); //Sama kuin "cout << "
+
+            int luku;
+
+            luku = int.Parse(Console.ReadLine()); //Sama kuin "cin >> "
+
+            Console.WriteLine("Annoit luvun: " + luku); //Sama kuin "cout << ", ja + on <<
+        }
+    }
+}
