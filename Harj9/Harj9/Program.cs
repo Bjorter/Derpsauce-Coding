d89b85e9f1818868bc6052c2e33f5bc809d2e155
Bjorter <bjorkman.tero@gmail.com>
2016-01-13 15:34:09 +0200

    Harjoituksia

---
+++ Harj9/Harj9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harj9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yhteen = 0;
+            for (int luku = 1; luku != 0;)
+            {
+                Console.WriteLine("Anna luku: ");
+                luku = int.Parse(Console.ReadLine());
+                yhteen = yhteen + luku;
+            }
+
+            Console.WriteLine("Luvut yhteensä: " + yhteen);
+        }
+    }
+}
