a5359d05fe1d643d2e592a20523b160b11d3da80
Bjorter <bjorkman.tero@gmail.com>
2016-01-19 13:23:36 +0200

    viikko 2 Läksy 3

---
+++ viikko2 harj6/viikko2 harj6/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace viikko2_harj6
+{
+    class Account
+    {
+        public double Funds;
+
+        public int Games;
+
+        public string Name;
+
+        public void PrintData()
+        {
+            Console.WriteLine("Acccount name: {0}\nYour funds: {1}\nNumber of games owned: {2}", Name, Funds, Games);
+        }
+
+    }
+}
---
+++ viikko2 harj6/viikko2 harj6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace viikko2_harj6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account();
+
+            account.Name = "Terppa";
+            account.Funds = 2.5;
+            account.Games = 234;
+
+            account.PrintData();
+        }
+    }
+}
+
+
