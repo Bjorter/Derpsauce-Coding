d1a9f7ed69e6e035144166bbfe442dcaf666c4f3
Bjorter <bjorkman.tero@gmail.com>
2016-01-19 12:16:14 +0200

    Viikko2 läksy2

---
+++ viikko2 harj5/viikko2 harj5/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace viikko2_harj5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Students[] students = new Students[5];
+
+            students[0] = new Students();
+
+            students[0].Name = "Tero";
+            students[0].Age = 24;
+            students[0].School = "JAMK";
+
+            students[1] = new Students();
+
+            students[1].Name = "Antti";
+            students[1].Age = 19;
+            students[1].School = "JAMK";
+
+            students[2] = new Students();
+
+            students[2].Name = "Balthazar";
+            students[2].Age = 230;
+            students[2].School = "JAMK";
+
+            students[3] = new Students();
+
+            students[3].Name = "Ragnarok";
+            students[3].Age = 5000;
+            students[3].School = "JAMK";
+
+            students[4] = new Students();
+
+            students[4].Name = "Kalja";
+            students[4].Age = 12390;
+            students[4].School = "JAMK";
+
+            for (int i = 0; i < 5; i++)
+            {
+                students[i].PrintData();
+            }
+        }
+    }
+}
---
+++ viikko2 harj5/viikko2 harj5/students.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace viikko2_harj5
+{
+    class Students
+    {
+        public string Name;
+        public int Age;
+        public string School;
+
+        public void PrintData()
+        {
+            Console.WriteLine("Name: {0}\nAge {1}\nSchool: {2}", Name, Age, School);
+        }
+    }
+}
