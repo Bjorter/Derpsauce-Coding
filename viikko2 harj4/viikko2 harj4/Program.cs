0a683f5c425f689c801feb71229e7803f7b1e667
Bjorter <bjorkman.tero@gmail.com>
2016-01-19 11:07:47 +0200

    Vko2 Läksy 1

---
+++ viikko2 harj4/viikko2 harj4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace viikko2_harj4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle vehicle = new Vehicle();
+
+            vehicle.Name = "Audi";
+            vehicle.Speed = 120;
+            vehicle.Tyres = 4;
+
+            vehicle.PrintData();
+            Console.WriteLine(vehicle.ToString());
+        }
+    }
+}
---
+++ viikko2 harj4/viikko2 harj4/Vehicle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace viikko2_harj4
+{
+    class Vehicle
+    {
+        public string Name;
+        public int Speed;
+        public int Tyres;
+
+        public void PrintData()
+        {
+            Console.WriteLine("Name: {0}\nSpeed: {1}\nTyres: {2}", Name, Speed, Tyres);
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + "\nSpeed: " + Speed + " km/h\nTyres: " + Tyres;
+        }
+    }
+
+
+}
